ad9695dbcb3db9f83be65c8e4f907ab225c4d4e9
[jkianese] <[jkianese@gmail.com]>
2018-10-04 10:52:12 -0400

    removed foursquare

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 class MyMap extends Component {
     
@@ -18,32 +18,10 @@ class MyMap extends Component {
     }
   
     loadMap = () => { 
-        scriptSrc();
+        // scriptSrc();
         window.initMap = this.initMap
     }
 
-    getVenues = () => {
-      const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-      const parameters = {
-        client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
-        client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
-        //section: "topPicks",
-        query: "arts",
-        ll: "40.448506, -80.002501",
-        limit: "10",
-        v: "20181012"
-      }
-      // npm install axios to run
-      axios.get(endPoint + new URLSearchParams(parameters))
-        .then(response => {
-          this.setState({
-            venues: response.data.response.groups[0].items
-          }, this.loadMap())
-        })
-        .catch(error => {
-          console.log("Error: " + error)
-        })
-    }
     
     initMap = () => {
 
@@ -88,15 +66,6 @@ class MyMap extends Component {
       </main>  
     )
   }
-}
-
-function scriptSrc() {
-  let index = window.document.getElementsByTagName("script")[0]
-  let script = window.document.createElement("script")
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap'
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
 }
 
 export default MyMap
