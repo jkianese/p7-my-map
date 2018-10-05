8567f44c7a76a3a88ce8629cf45d7f8c6fa67ae0
[jkianese] <[jkianese@gmail.com]>
2018-10-05 07:35:13 -0400

    update variables to ES6

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -22,53 +22,28 @@ class MyMap extends Component {
 
   componentDidMount() {
     this.getVenues()
-    // this.loadMap()
     window.initMap = this.initMap
   }
  
-  getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-    const parameters = {
-      client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
-      client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
-      query: "arts", 
-      near: "Pittsburgh, PA",
-      //ll: "40.448506, -80.002501",
-      limit: 10,
-      v: "20181005"
-    }
-
-    // From Walthrough video series
-    // Run: npm install axios
-    axios.get(endPoint + new URLSearchParams(parameters)) //URLSearchParams is actual function, don't change
-      .then(response => {
-        this.setState({
-          venues: response.data.response.groups[0].items
-        }, loadScript())
-      })
-      .catch(error => {
-        console.log("Error: " + error)
-      })
-
-  }
-
   initMap = () => {
 
+    const google = window.google
+
     // create a map 
-    const map = new window.google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.448506, lng: -80.002501},
       zoom: 12
     })
     
     // create an infowindow
-    var infowindow = new window.google.maps.InfoWindow()
+    let infowindow = new google.maps.InfoWindow()
 
     // display dynamic markers
     this.state.venues.map(myVenue => {
 
-    var contentString = `${myVenue.venue.name}` // Removed Place Name in WT 
+    let contentString = `${myVenue.venue.name}` // Removed Place Name in WT 
 
-    var marker = new window.google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
       map: map,
       title: myVenue.venue.name,
@@ -88,7 +63,33 @@ class MyMap extends Component {
     
   });
   
-}      
+}
+
+getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
+      client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
+      query: "arts", 
+      near: "Pittsburgh, PA",
+      //ll: "40.448506, -80.002501",
+      limit: 10,
+      v: "20181005"
+    }
+
+    // From Walthrough video series
+    // Run: npm install axios
+    axios.get(endPoint + new URLSearchParams(parameters)) //URLSearchParams is actual function, don't change
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, scriptSrc())
+      })
+      .catch(error => {
+        console.log("Error: " + error)
+      })
+
+  }
   
   render() { 
     return (
@@ -99,7 +100,7 @@ class MyMap extends Component {
   }
 }
 
-function loadScript() {
+function scriptSrc() {
     let index = window.document.getElementsByTagName("script")[0]
     let script = window.document.createElement("script")
     script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap'
